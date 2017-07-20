dbbcbbcc4309e3839f52263ba0805a612320e320
Milan <shah.milan.p@gmail.com>
2017-07-20 00:06:57 -0400

    base App

--- ui/onboarding-live/src/app/app.module.ts
+++ ui/onboarding-live/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { MenuComponent } from './common/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule

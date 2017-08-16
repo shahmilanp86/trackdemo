ea28433db626c032dd521417e31800830a815fc8
sreeramrs <sreeram.rs@outlook.com>
2017-08-16 01:16:46 -0400

    Updated dashboard template and component

--- ui/onboarding-live/src/app/pages/dashboard/dashboard.component.ts
+++ ui/onboarding-live/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  onBoardingDetails: OnBoardingDetail[];
 
-  constructor() { }
+  constructor() { 
+    this.onBoardingDetails = [
+      new OnBoardingDetail('e464649','Robert Will', 'Candidate to be informed', 'N/A', null, 'Today', 'N/A', null),
+      new OnBoardingDetail('e464641','Tom Edward', 'Candidate info level 1 needed', 'N/A', null, 'Today', 'N/A', null),
+      new OnBoardingDetail('t464649','Sumit Adule', 'Awaiting BG and Demographics', 'Awaiting Vendor', 'Complete', '2 days', '9918105401', null),
+      new OnBoardingDetail('h464649','Lin Jin', 'Candidate to be informed', 'N/A', null, 'Today', '2809501253', null),
+    ];
+  }
 
   ngOnInit() {
+  }
+
+}
+
+class OnBoardingDetail{
+
+  constructor(
+    public Aid: string,
+    public Name: string,
+    public Status: string,
+    public BGCheck: string,
+    public Demographics: string,
+    public SLA: string,
+    public Contact: string,
+    public Notes: string
+  ){
   }
 
 }

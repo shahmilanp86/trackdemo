c61462854bdb5b664b322f1fc8449f0312d3d7c4
gauravsaxena <gauravsaxena198@yahoo.com>
2017-09-03 14:03:19 -0400

    Added new files for sequential form

---
+++ ui/onboarding-live/src/app/pages/idrf/address/address.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit }   from '@angular/core';
+
+import { Address }             from '../data/formData.model';
+import { FormDataService }     from '../data/formData.service';
+
+@Component ({
+    selector:     'mt-wizard-address'
+    ,templateUrl: './address.component.html'
+})
+
+export class AddressComponent implements OnInit {
+    title = 'Where do you live?';
+    address: Address;
+    form: any;
+
+    constructor(private formDataService: FormDataService) {
+    }
+
+    ngOnInit() {
+        this.address = this.formDataService.getAddress();
+        console.log('Address feature loaded!');
+    }
+
+    save(form: any) {
+        if (!form.valid)
+            return;
+
+        this.formDataService.setAddress(this.address);
+    }
+}
---
+++ ui/onboarding-live/src/app/pages/idrf/work/work.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit }   from '@angular/core';
+
+import { FormDataService }     from '../data/formData.service';
+
+@Component ({
+    selector:     'mt-wizard-work'
+    ,templateUrl: './work.component.html'
+})
+
+export class WorkComponent implements OnInit {
+    title = 'What do you do?';
+    workType: string;
+    form: any;
+
+    constructor(private formDataService: FormDataService) {
+    }
+
+    ngOnInit() {
+        this.workType = this.formDataService.getWork();
+        console.log('Work feature loaded!');
+    }
+
+    save(form: any) {
+        if (!form.valid)
+            return;
+
+        this.formDataService.setWork(this.workType);
+    }
+}
